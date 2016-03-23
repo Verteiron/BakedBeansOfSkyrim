4ff2ed99b1b91eca077d82b23b8e84510c859b7c
Verteiron <verteiron@gmail.com>
2016-03-23 12:36:21 -0500

    Update build scripts.

--- dist/build_7z.cmd
+++ dist/build_7z.cmd
@@ -12,10 +12,21 @@ xcopy /y ..\data\*.esp .
 xcopy /y ..\data\*.bsa .
 xcopy /y ..\data\*readme* .
 xcopy /y ..\..\doc\*readme* .
-echo d | xcopy /e /y ..\data\skse skse
-echo d | xcopy /e /y ..\data\vBBS vBBS
-"%ZEXE%" a -r "vBBS_BakedBeans_%NEWTIME%.7z" "*"
+if exist ..\data\skse ( echo d | xcopy /e /y ..\data\skse skse )
+"%ZEXE%" a -r "%FILENAME%_%NEWTIME%_%COMPUTERNAME%.7z" "*"
 xcopy /y *.7z ..
 cd ..
+set I=0
+echo Removing temp files, this may take a sec...
+:DeleteTemp
+REM Wait 1 second
 ping -n 1 -w 1000 1.0.0.0 > nul
 rmdir /s /q "%NEWTIME%_%COMPUTERNAME%"
+set /A I += 1
+echo Try %I%
+if %I% GTR 5 goto NoDelete
+if exist "%NEWTIME%_%COMPUTERNAME%" goto DeleteTemp
+goto End
+:NoDelete
+echo Couldn't remove %NEWTIME%_%COMPUTERNAME% directory, delete it yourself!
+:End
